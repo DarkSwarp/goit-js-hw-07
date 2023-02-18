95a03872aff5636a7a537dc8703ccf056475a45d
DarkSwarp <mihkleban@gmail.com>
2023-02-18 16:42:05 +0200

    all tasks add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
+const modalBox = document.querySelector(".basicLightbox");
 
-console.log(galleryItems);
+// Створення розмітки галереї
+
+const galleryMarkup = galleryItems
+    .map(
+        ({ preview, original, description }) =>
+            `<div class="gallery__item"><a class = "gallery__link" href = "${original}" target = "_self"><img class = "gallery__image" src = "${preview}" data-source = "${original}" alt = "${description}"></a></div>`
+    )
+    .join("");
+galleryBox.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// Відкриття модалки
+
+galleryBox.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+
+    // Початок прослуховування натискання клавіш
+
+    window.addEventListener("keydown", escapePressCheck);
+
+    // Перевірка натискання кнопки Escape та закриття модалки та закінчення прослуховування натискання клавіш при наявності Escape
+
+    function escapePressCheck(event) {
+        if (event.code !== "Escape") {
+            return;
+        }
+        instance.close();
+        window.removeEventListener("keydown", escapePressCheck);
+    }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
 
-console.log(galleryItems);
+// Створення розмітки галереї
+
+const galleryMarkup = galleryItems
+    .map(
+        ({ preview, original, description }) =>
+            `<a class = "gallery__item" href = "${original}" target = "_self"><img class = "gallery__image" src = "${preview}" alt = "${description}"></a>`
+    )
+    .join("");
+
+galleryBox.insertAdjacentHTML("beforeend", galleryMarkup);
+const lightbox = new SimpleLightbox(".gallery a", { captionType: "attr", captionsData: "alt", captionPosition: "bottom", captionDelay: 250 });
